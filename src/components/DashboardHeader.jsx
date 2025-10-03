920fde79c4711f5b2460a1b7d262156f45ceb2c5
David Aniede <ndubest56@gmail.com>
2025-10-03 14:45:22 +0100

    balance decimal

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -1,108 +1,116 @@
-import React, { useEffect, useState } from 'react'
-import './compstyle.css'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import "./compstyle.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function DashboardHeader() {
-  const navigate = useNavigate()
-  const [balance, setBalance] = useState(0)
-  const [copy, setCopy] = useState(false)
+  const navigate = useNavigate();
+  const [balance, setBalance] = useState(0);
+  const [copy, setCopy] = useState(false);
 
   useEffect(() => {
     fetch(`https://firsttechwallet.top/macdon/get_last_balance.php`)
       .then((res) => res.json())
       .then((data) => {
         if (data.balance) {
-          setBalance(data.balance)
-          console.log(data.balance)
+          setBalance(data.balance);
+          console.log(data.balance);
         } else {
-          setBalance(0)
+          setBalance(0);
         }
       })
       .catch((err) => {
-        console.error('Failed to fetch balance:', err)
-        setBalance(0)
-      })
-  }, [])
+        console.error("Failed to fetch balance:", err);
+        setBalance(0);
+      });
+  }, []);
 
   function handleCopy() {
-    navigator.clipboard.writeText('3016487936').then(() => {
-      setCopy(true)
+    navigator.clipboard.writeText("3016487936").then(() => {
+      setCopy(true);
       setTimeout(() => {
-        setCopy(false)
-      }, 2000).catch((err) => console.log('Failed to copy: ', err))
-    })
+        setCopy(false);
+      }, 2000).catch((err) => console.log("Failed to copy: ", err));
+    });
   }
 
   const logout = async () => {
-    await axios.get('https://firsttechwallet.top/macdon/logout.php')
-    navigate('/')
-  }
+    await axios.get("https://firsttechwallet.top/macdon/logout.php");
+    navigate("/");
+  };
   return (
-    <div className='dashheader'>
-      <section className='upper'>
-        <div className='text-sm user different '>
-          <img src='icons/camera.png' alt='' />
+    <div className="dashheader">
+      <section className="upper">
+        <div className="text-sm user different ">
+          <img src="icons/camera.png" alt="" />
           <div>
-            <p className='pt-5 font-bold'>
-              <span className='text-blue-900'>Welcome</span> ABANA WAKIR Mohammed
+            <p className="pt-5 font-bold">
+              <span className="text-blue-900">Welcome</span> ABANA WAKIR
+              Mohammed
             </p>
             <p
               onClick={handleCopy}
-              className='text-lg account-num text-blue-700 font-bold mb-1 underline cursor-pointer'
+              className="text-lg account-num text-blue-700 font-bold mb-1 underline cursor-pointer"
             >
-              Account: {copy ? 'copied' : 3016487936}
+              Account: {copy ? "copied" : 3016487936}
             </p>
           </div>
-          <p className='balance'>
-            <span>Balance:</span> {balance}
+          <p className="balance">
+            <span>Balance:</span> ₦
+            {Number(balance).toLocaleString("en-NG", {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
           </p>
         </div>
 
-        <section className='user-account text-sm'>
+        <section className="user-account text-sm">
           <p
             onClick={handleCopy}
-            className='account-number text-blue-700 font-bold mb-1 underline cursor-pointer'
+            className="account-number text-blue-700 font-bold mb-1 underline cursor-pointer"
           >
-            Account: {copy ? 'copied' : 3016487936}
+            Account: {copy ? "copied" : 3016487936}
           </p>
-          <button className='logoutbtn' onClick={logout}>
+          <button className="logoutbtn" onClick={logout}>
             Logout
           </button>
         </section>
       </section>
-      <div className='nav-container'>
+      <div className="nav-container">
         <nav>
-          <div onClick={() => navigate('/dashboard')}>
-            <img src='icons/homesub.png' alt='' />
+          <div onClick={() => navigate("/dashboard")}>
+            <img src="icons/homesub.png" alt="" />
             <span>Home</span>
           </div>
-          <div onClick={() => navigate('/dashboardform')}>
-            <img src='icons/transfer.png' alt='' />
+          <div onClick={() => navigate("/dashboardform")}>
+            <img src="icons/transfer.png" alt="" />
             <span>Tranfer</span>
           </div>
           <div>
-            <img src='icons/loan.png' alt='' />
+            <img src="icons/loan.png" alt="" />
             <span>Loans</span>
           </div>
           <div>
-            <img src='icons/airtime.png' alt='' />
+            <img src="icons/airtime.png" alt="" />
             <span>Airtime</span>
           </div>
           <div>
-            <img src='icons/card.png' alt='' />
+            <img src="icons/card.png" alt="" />
             <span>Cards</span>
           </div>
           {/* <div>
           <img src='/public/icons/code.png' alt='' />
           <span>QR</span>
         </div> */}
-          <div onClick={() => navigate('/all-transactions')} className='different'>
-            <img src='icons/history.png' alt='' />
+          <div
+            onClick={() => navigate("/all-transactions")}
+            className="different"
+          >
+            <img src="icons/history.png" alt="" />
             <span>History</span>
           </div>
         </nav>
       </div>
     </div>
-  )
+  );
 }
